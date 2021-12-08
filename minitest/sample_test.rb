8877a8cde6471c27d36587cdd31e10fb79934a34
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-08 23:42:51 +0900

    リファクタリング

---
+++ minitest/sample_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+
+class SampletTest < Minitest::Test
+  def test_sample
+    assert_equal 'RUBY', 'hoge'.upcase
+    assert_equal 'RUBY', nil.upcase
+  end
+
+  def test_hoge
+    assert_equal 'RUBY', 'ruby'.upcase
+    assert_equal 'RUBY', 'hoge'.upcase
+  end
+end
